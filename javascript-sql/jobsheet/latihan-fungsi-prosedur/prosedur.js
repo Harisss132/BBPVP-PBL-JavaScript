df5c53c5e4c80839d8d7eab140410418a97ec41c
Harisss132 <zaktwod123@gmail.com>
2025-08-12 16:21:54 +0700

    first commit

---
+++ javascript-sql/jobsheet/latihan-fungsi-prosedur/prosedur.js
@@ -0,0 +1,8 @@
+function cetakbiodata (nama, umur, hobi, alamat) {
+    console.log("Nama   :", nama);
+    console.log("Umur   :", umur);
+    console.log("Hobi   :", hobi);
+    console.log("Alamat :", alamat);
+}
+
+cetakbiodata("Gardenia", 20, "nyanyi", "Jl. Gumtur");
